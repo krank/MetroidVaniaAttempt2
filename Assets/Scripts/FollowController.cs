24e8012cd42a7f59f1fae3b6f719dccf7ef02dc2
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2025-10-25 14:18:02 +0200

    Some restructuring

--- Assets/Scripts/FollowController.cs
+++ Assets/Scripts/FollowController.cs
@@ -1,80 +1,77 @@
 using UnityEngine;
 
-namespace Vania3D
+public class FollowController : MonoBehaviour
 {
-  public class FollowController : MonoBehaviour
+  [SerializeField]
+  Transform targetObject;
+
+  [SerializeField]
+  Transform origoObject;
+
+  [Header("Following")]
+  [SerializeField]
+  bool followX = true;
+  [SerializeField]
+  bool followY = true;
+
+  [SerializeField]
+  float maxSpeed = 1;
+
+  [Header("Easing")]
+  [SerializeField]
+  float maxEasingDistance = 3;
+  [SerializeField]
+  AnimationCurve easingCurve;
+
+  Vector3 offsetLastFrame;
+
+  void Start()
   {
-    [SerializeField]
-    Transform targetObject;
+    SetOffset();
+  }
 
-    [SerializeField]
-    Transform origoObject;
+  void Update()
+  {
+    ApplyOffset();
 
-    [Header("Following")]
-    [SerializeField]
-    bool followX = true;
-    [SerializeField]
-    bool followY = true;
+    Vector3 targetPos = new(
+      followX ? targetObject.position.x : transform.position.x,
+      followY ? targetObject.position.y : transform.position.y,
+      transform.position.z
+    );
 
-    [SerializeField]
-    float maxSpeed = 1;
+    // Difference between where we want to be and where we actually are
+    Vector3 diff = new(
+      followX ? transform.position.x - targetObject.position.x : 0,
+      followY ? transform.position.y - targetObject.position.y : 0
+    );
 
-    [Header("Easing")]
-    [SerializeField]
-    float maxEasingDistance = 3;
-    [SerializeField]
-    AnimationCurve easingCurve;
+    float distanceBetween = Mathf.Min(maxEasingDistance, diff.magnitude);
 
-    Vector3 offsetLastFrame;
+    // Acceleration based on how far aling the maxEasingDistance we've gotten
+    float acceleration = easingCurve.Evaluate(1f - (distanceBetween / maxEasingDistance));
 
-    void Start()
-    {
-      SetOffset();
-    }
+    transform.position = Vector3.MoveTowards(
+      transform.position,
+      targetPos,
+      maxSpeed * acceleration * Time.deltaTime
+    );
 
-    void Update()
-    {
-      ApplyOffset();
+    SetOffset();
+  }
 
-      Vector3 targetPos = new(
-        followX ? targetObject.position.x : transform.position.x,
-        followY ? targetObject.position.y : transform.position.y,
-        transform.position.z
-      );
+  void SetOffset()
+  {
+    offsetLastFrame = transform.position - origoObject.position;
+  }
 
-      // Difference between where we want to be and where we actually are
-      Vector3 diff = new(
-        followX ? transform.position.x - targetObject.position.x : 0,
-        followY ? transform.position.y - targetObject.position.y : 0
-      );
-
-      float distanceBetween = Mathf.Min(maxEasingDistance, diff.magnitude);
-
-      // Acceleration based on how far aling the maxEasingDistance we've gotten
-      float acceleration = easingCurve.Evaluate(1f - (distanceBetween / maxEasingDistance));
-
-      transform.position = Vector3.MoveTowards(
-        transform.position,
-        targetPos,
-        maxSpeed * acceleration * Time.deltaTime
-      );
-
-      SetOffset();
-    }
-
-    void SetOffset()
-    {
-      offsetLastFrame = transform.position - origoObject.position;
-    }
-
-    void ApplyOffset()
-    {
-      // transform.position = origoObject.position + offsetLastFrame;
-      transform.position = new(
-        followX ? origoObject.position.x + offsetLastFrame.x : transform.position.x,
-        followY ? origoObject.position.y + offsetLastFrame.y : transform.position.y,
-        transform.position.z
-      );
-    }
+  void ApplyOffset()
+  {
+    // transform.position = origoObject.position + offsetLastFrame;
+    transform.position = new(
+      followX ? origoObject.position.x + offsetLastFrame.x : transform.position.x,
+      followY ? origoObject.position.y + offsetLastFrame.y : transform.position.y,
+      transform.position.z
+    );
   }
 }
--- Assets/Scripts/LookaheadTargetController.cs
+++ Assets/Scripts/LookaheadTargetController.cs
@@ -1,53 +1,50 @@
 using UnityEngine;
-namespace Vania3D
+public class LookaheadTargetController : MonoBehaviour
 {
-  public class LookaheadTargetController : MonoBehaviour
+
+  [SerializeField]
+  bool followX = true;
+
+  [SerializeField]
+  bool followY = true;
+
+  [SerializeField]
+  float distance = 4;
+
+  [SerializeField]
+  bool centerWhenStill = true;
+
+  CharacterController charController;
+  Vector3 targetPosition;
+
+  void Start()
   {
+    charController = transform.parent.GetComponent<CharacterController>();
+  }
 
-    [SerializeField]
-    bool followX = true;
+  void Update()
+  {
+    Vector3 direction = new Vector3(
+      followX ? charController.velocity.x : 0,
+      followY ? charController.velocity.y : 0
+    ).normalized;
 
-    [SerializeField]
-    bool followY = true;
+    bool currentlyMoving =
+      (followX && charController.velocity.x != 0) ||
+      (followY && charController.velocity.y != 0);
 
-    [SerializeField]
-    float distance = 4;
-
-    [SerializeField]
-    bool centerWhenStill = true;
-
-    CharacterController charController;
-    Vector3 targetPosition;
-
-    void Start()
+    // Activity when moving
+    if (currentlyMoving)
     {
-      charController = transform.parent.GetComponent<CharacterController>();
+      targetPosition = direction * distance;
     }
 
-    void Update()
+    // Activity at rest
+    else if (centerWhenStill)
     {
-      Vector3 direction = new Vector3(
-        followX ? charController.velocity.x : 0,
-        followY ? charController.velocity.y : 0
-      ).normalized;
-
-      bool currentlyMoving =
-        (followX && charController.velocity.x != 0) ||
-        (followY && charController.velocity.y != 0);
-
-      // Activity when moving
-      if (currentlyMoving)
-      {
-        targetPosition = direction * distance;
-      }
-
-      // Activity at rest
-      else if (centerWhenStill)
-      {
-        targetPosition = Vector3.zero;
-      }
-
-      transform.localPosition = targetPosition;
+      targetPosition = Vector3.zero;
     }
+
+    transform.localPosition = targetPosition;
   }
 }
